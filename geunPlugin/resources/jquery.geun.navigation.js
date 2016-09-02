6c5f7cff2bb77ac876a1091e1952834a4d55d345
kikuen <hap0p9y@nate.com>
2016-09-02 10:31:54 +0900

    전체 추가
    전체 보기 추가

--- geunPlugin/resources/jquery.geun.navigation.js
+++ geunPlugin/resources/jquery.geun.navigation.js
@@ -2,9 +2,10 @@
                 
    var pluginName ="geunPlugin",
        defaults = { 
-           width:1100,               //navigation 넓이
+           width:1100,              //navigation 넓이
            height:50,               //navigation 높이
-           time:300
+           time:200,                //navigation drop&down timer
+           dropType:"all"           //navigation drop&down type menu all default stript
        };
 
     function Plugin(element,options){
@@ -12,31 +13,70 @@
         this.options = $.extend({},defaults,options);
         this._defaults = defaults;
         this._name = pluginName;
-        genuNavInitSize(element,this.options);
+        geunNavInitSize(element,this.options);
         this.init();
     }
 
-    function genuNavInitSize(selector,options){
+    function geunNavInitSize(selector,options){
+        
         $(selector).css("position","relative");
         var thisIndex = $(selector).find("ul").find("li:last-child").index();
-        
-        
         var widthLi = options.width / (thisIndex+1);
         $(selector).find("ul").find("li").css("width",widthLi).css("height",options.height).css("line-height",options.height+"px");
         
-        /*var widthLi = 100 / (thisIndex+1);*/
+        /*
+        var widthLi = 100 / (thisIndex+1);
+        
+        $(selector).find("ul").find("li").css("width",widthLi+"%").css("height",options.height).css("line-height",options.height+"px");
+        */
+       
+        $(".geunSubDiv").css("width",widthLi);
+        $(".geunSpanWrap").css("width","100%");
+        $(selector).find("ul").filter(function(){
+            $(this).css("width",options.width).css("height",options.height);
+        }); 
+        
+        switch(options.dropType){
+            case("stript") :
+                geunStriptInit(selector,options);
+                break;
+            case("all") : 
+                geunAllInit(selector,options);
+                break;
+            default : 
+                geunStriptInit(selector,options);
+                break;
+        }
+    }
+    
+    function geunStriptInit(selector,options){
+        
         $(selector).find("ul").find("li").mouseover(function(){
             $(this).find(".geunSubDiv").stop().slideDown(options.time);
         }).mouseleave(function(){
             $(this).find(".geunSubDiv").stop().slideUp(options.time);
         });
-        $(".geunSubDiv").css("width",widthLi);
-        $(".geunSpanWrap").css("width","100%");
-        /*$(selector).find("ul").find("li").css("width",widthLi+"%").css("height",options.height).css("line-height",options.height+"px");*/
-        $(selector).find("ul").filter(function(){
-            $(this).css("width",options.width).css("height",options.height);
-        });
+        
     }
+    
+     function geunAllInit(selector,options){
+         var geunSubDivHeight = 0;
+         $(selector).find(".geunSubDiv").each(function(){
+             if(geunSubDivHeight < $(this).height()){
+                geunSubDivHeight = $(this).height();    
+             }
+         });
+         
+        $(".geunSubDiv").height(geunSubDivHeight);  
+         
+        $(selector).find("ul").find("li").mouseover(function(){
+            $(".geunSubDiv").stop().slideDown(options.time);
+        }).mouseleave(function(){
+            $(".geunSubDiv").stop().slideUp(options.time);
+        });
+         
+    }
+    
 
     Plugin.prototype ={
         init : function(){
@@ -50,5 +90,3 @@
     };
 
 })(jQuery,window,document);   
-
-
